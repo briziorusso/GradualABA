970b98901c7549fb0f4ff46514eecb7d8b1e6867
briziorusso <brizio.russo@gmail.com>
2025-05-01 22:26:45 +0100

    test for cont dups and for argument comparison to asp

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import unittest
+import os, sys
 
 from BSAF.BSAF import BSAF
 from BSAF.Argument import Argument
@@ -6,6 +7,7 @@ from ABAF.Assumption import Assumption
 from ABAF.Rule import Rule
 from ABAF.Sentence import Sentence
 from ABAF.ABAF import ABAF
+from semantics.modular.SetProductAggregation import SetProductAggregation 
 
 class TestBSAF(unittest.TestCase):
 
@@ -205,7 +207,7 @@ class TestABAF(unittest.TestCase):
         r 6 2 3        
         """
         
-        iccma_file = "examples/ABA_ICCMA_input.iccma"
+        iccma_file = f"{os.path.dirname(os.path.realpath(__file__))}/examples/ABA_ICCMA_input.iccma"
         # Assuming the file contains valid ICCMA format
         abaf = ABAF(path=iccma_file)
         # Check if the assumptions and rules are parsed correctly
@@ -220,7 +222,7 @@ class TestABAF(unittest.TestCase):
         ## format assumtions and rules
         actual_assumptions = {assumption.name for assumption in abaf.assumptions}
         actual_contraries = {assumption.name: assumption.contrary for assumption in abaf.assumptions}
-        actual_rules = {rule.head.name: set(rule.body) for rule in abaf.rules}
+        actual_rules = {rule.head.name: set([b.name for b in rule.body]) for rule in abaf.rules}
 
         # Check assumptions
         self.assertEqual(actual_assumptions, expected_assumptions, f"Expected assumptions: {expected_assumptions}, but got: {set(abaf.assumptions)}")
@@ -229,8 +231,117 @@ class TestABAF(unittest.TestCase):
             self.assertEqual(actual_contraries[assumption.name], expected_contraries[assumption.name], f"Expected contrary for {assumption.name}: {expected_contraries[assumption.name]}, but got: {assumption.contrary}")
         # Check rules
         for rule in abaf.rules:
-            self.assertEqual(actual_rules[rule.head.name], expected_rules[rule.head.name], f"Expected rule for {rule.head.name}: {expected_rules[rule.head.name]}, but got: {abaf.rules[rule.head.name]}")
+            self.assertEqual(actual_rules[rule.head.name], expected_rules[rule.head.name], f"Expected rule for {rule.head.name}: {expected_rules[rule.head.name]}, but got: {actual_rules[rule.head.name]}")
         print("ABAF loaded from ICCMA file correctly.")
+
+    def test_argument_compare(self):
+        """Load an ICCMA file and convert it to ABAF format.
+        # Example. The ABA framework with rules 
+        # p :- q,a.
+        # q :- .
+        # r :- b,c. 
+        # assumptions a,b,c
+        # contraries a̅ = r, b̅ = s, c̅ = t 
+        # is specified as follows, with atom-indexing a=1, b=2, c=3, p=4, q=5, r=6, s=7, t=8.
+
+        p aba 8
+        a 1
+        a 2
+        a 3
+        c 1 6
+        c 2 7
+        c 3 8
+        r 4 5 1
+        r 5
+        r 6 2 3        
+        """
+        
+        iccma_file = "examples/ABA_ICCMA_input.iccma"
+        # Assuming the file contains valid ICCMA format
+        abaf = ABAF(path=iccma_file, arg_mode='prune_supersets')
+        # Check if the assumptions and rules are parsed correctly
+
+        ## Compare arguments to derived using cling
+        abaf._build_arguments(SetProductAggregation())
+
+        ### extract all the derived atoms from ASP and compare to the arguments
+        args_claims = set()
+        for atom in abaf.derived_in:
+            args_claims.add(atom)
+        
+        ## build the arguments
+        abaf.build_arguments_procedure(SetProductAggregation())
+
+        ## Format the arguments
+        args = set()
+        for arg in abaf.arguments:
+            args.add(arg.claim.name)
+
+        # Check that abaf.derived == {abaf.arguments}
+        self.assertEqual(args_claims, args, f"Expected derived arguments: {abaf.arguments}, but got: {abaf.derived}")
+        print("Arguments derived procedurally and from ASP are the same.")
+
+
+    def test_argument_compare2(self):
+        """Load an ICCMA file and convert it to ABAF format.
+        # Example. The ABA framework with rules 
+        # p :- q,a.
+        # q :- .
+        # r :- b,c. 
+        # assumptions a,b,c
+        # contraries a̅ = r, b̅ = s, c̅ = t 
+        # is specified as follows, with atom-indexing a=1, b=2, c=3, p=4, q=5, r=6, s=7, t=8.
+
+        p aba 8
+        a 1
+        a 2
+        a 3
+        c 1 6
+        c 2 7
+        c 3 8
+        r 4 5 1
+        r 5
+        r 6 2 3        
+        """
+        
+        for i in range(0, 4):
+            iccma_file = f"../dependency-graph-alternative/input_data_nf/non_flat_1_s25_c0.02_n0.2_a0.3_r5_b5_{i}.aba"
+            # Assuming the file contains valid ICCMA format
+            abaf = ABAF(path=iccma_file, arg_mode='prune_supersets')
+            # Check if the assumptions and rules are parsed correctly
+
+            ## Compare arguments to derived using cling
+            abaf._build_arguments(SetProductAggregation())
+
+            ### extract all the derived atoms from ASP and compare to the arguments
+            args_claims = set()
+            for atom in abaf.derived_in:
+                args_claims.add(atom)
+            ## collect the premises from the ASP procedure
+            # args_premises = []
+            # for counter in range(1,abaf.arg_counter):
+            #     args_premises.append(abaf.derives[counter])
+
+            
+            ## build the arguments
+            abaf.build_arguments_procedure(SetProductAggregation())
+
+            ## Format the arguments
+            args_claims2 = set()
+            for arg in abaf.arguments:
+                args_claims2.add(arg.claim.name)
+            ## collect the premises from the arguments
+            # args_premises2 = []
+            # for arg in abaf.arguments:
+            #     args_premises2.append(arg.premise)
+            ## check the premises of the arguments - see if they are the same
+
+            # Check that abaf.derived == {abaf.arguments}
+            self.assertEqual(args_claims, args_claims2, f"Expected derived arguments: {abaf.arguments}, but got: {abaf.derived}")
+            ## Check that the premises are the same
+            # self.assertEqual(args_premises, args_premises2, f"Expected derived arguments: {args_premises}, but got: {args_premises2}")
+            print("Arguments derived procedurally and from ASP are the same.")
+
 
 
     def test_abaf_from_iccma_file2(self):
@@ -259,45 +370,28 @@ class TestABAF(unittest.TestCase):
         """
         
         iccma_file = "examples/ex2.iccma"
-        # Assuming the file contains valid ICCMA format
-        abaf = ABAF(path=iccma_file)
-        # Check if the assumptions and rules are parsed correctly
+        
+        ### Catch that the ABAF errors because of multiple contraries
+        try:
+            abaf = ABAF(path=iccma_file)
+        except Exception as e:
+            print(f"ABAF cannot be built: {e}")
+            self.assertTrue(True)
+        else:
+            self.assertTrue(False, "Expected an error when loading ABAF from ICCMA file with multiple contraries, but no error was raised.")
 
-        expected_assumptions = {'1', '2', '3', '4', '5'}
-        expected_contraries = {
-            '1': '2',
-            '2': '1',
-            '3': '4',
-            '4': '3',
-            '4': '2',
-            '2': '4',
-            '3': '4',
-            '5': '1',
-            '1': '5'
-        }
-        expected_rules = {
-            '6': {'2', '3', '5'},
-            '4': set()
-        }
-        ## format assumtions and rules
-        actual_assumptions = {assumption.name for assumption in abaf.assumptions}
-        actual_contraries = {assumption.name: assumption.contrary for assumption in abaf.assumptions}
-        actual_rules = {rule.head.name: set(rule.body) for rule in abaf.rules}
-        # Check assumptions
-        self.assertEqual(actual_assumptions, expected_assumptions, f"Expected assumptions: {expected_assumptions}, but got: {set(abaf.assumptions)}")
-        # Check contraries
-        for assumption in abaf.assumptions:
-            self.assertEqual(actual_contraries[assumption.name], expected_contraries[assumption.name], f"Expected contrary for {assumption.name}: {expected_contraries[assumption.name]}, but got: {assumption.contrary}")
-        # Check rules
-        for rule in abaf.rules:
-            self.assertEqual(actual_rules[rule.head.name], expected_rules[rule.head.name], f"Expected rule for {rule.head.name}: {expected_rules[rule.head.name]}, but got: {abaf.rules[rule.head.name]}")
-        print("ABAF loaded from ICCMA file correctly.")
+
+    ### attacks and supports correspont to arguments deriving assumptions and contraries of assumptions
+
+    ### Check for FLAT and non-FLAT
         
         
 TestBSAF().test_aba_bsaf_1()
 TestBSAF().test_aba_bsaf_2()
 TestABAF().test_abaf_from_iccma_file()
 TestABAF().test_abaf_from_iccma_file2()
+TestABAF().test_argument_compare()
+TestABAF().test_argument_compare2()
 
 print("All tests passed.")
 
